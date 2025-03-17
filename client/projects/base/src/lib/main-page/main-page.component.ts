118b251745d532b7f24544fd4b6527d10d311e47
juliadabrowka <julia0dabrowka@gmail.com>
2025-03-17 12:19:45 +0100

    chore: fix action bar display

--- client/projects/base/src/lib/main-page/main-page.component.ts
+++ client/projects/base/src/lib/main-page/main-page.component.ts
@@ -1,8 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {SfActionBarComponent} from '../action-bar/action-bar.component';
 
 @Component({
   selector: 'sf-main-page',
-  imports: [],
+  imports: [
+    SfActionBarComponent
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
